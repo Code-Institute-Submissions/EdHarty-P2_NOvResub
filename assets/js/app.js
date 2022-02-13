46339659e73bc3dce751dfb805c3b481cc3e542f
EdHarty <eharty1@outlook.com>
2022-02-13 09:36:58 +0000

    add start game function

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,13 @@ const game = function () {
     let pScore = 0;
     let cScore = 0;
 }
-  
+const startGame = function() {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", function () {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+};
